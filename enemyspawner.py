e2fbc72ed2fb76a081ff7dbae1f73d861a36f503
Radmaster5000 <phil.radford5000@gmail.com>
2020-08-19 22:17:12 +0100

    Added demon enemy type

--- enemyspawner.py
+++ enemyspawner.py
@@ -6,3 +6,10 @@ def spawn_zombies(number_of_enemies):
 		zombie = character('enemy', 'zombie', 'undead zombie', 3, 10, 5, 0, [])
 		array_of_zombies.append(zombie)
 	return array_of_zombies
+
+def spawn_demons(number_of_enemies):
+	array_of_demons = []
+	for i in range(number_of_enemies):
+		demon = character('enemy', 'monster', 'demon', 10, 20, 10, 5, [])
+		array_of_demons.append(demon)
+	return array_of_demons
