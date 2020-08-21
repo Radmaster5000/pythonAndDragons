28217e5b7a7a65f51cc0ade6f7382b91a00cde80
Radmaster5000 <phil.radford5000@gmail.com>
2020-08-21 21:44:27 +0100

    cleaned up some of the testing code. Stripped some of the unnecessary variables from the classes and started to implement a scene loading function

--- classes.py
+++ classes.py
@@ -1,7 +1,6 @@
 class character: 
-	def __init__(self, type, subtype, name, strength, health, speed, magic, equipment):
+	def __init__(self, type, name, strength, health, speed, magic, equipment):
 		self.type = type
-		self.subtype = subtype
 		self.name = name
 		self.strength = strength
 		self.health = health
@@ -10,24 +9,22 @@ class character:
 		self.equipment = equipment
 		
 class location:
-	def __init__(self, address, status, name, characters, list_of_enemies, items, description, possible_directions):
-		self.address = address
-		self.status = status
+	def __init__(self, name, characters, list_of_enemies, items, description):
 		self.name = name
 		self.characters = characters
 		self.list_of_enemies = list_of_enemies
 		self.items = items
 		self.description = description
-		self.possible_directions = possible_directions
 		
 
 #########################################
 # CHARACTER LIBRARY
 
-tavern_owner = character('NPC', 'human', 'Tavern Owner', 1000, 1000, 1000, 0, ['large knife'])
-player = character('player character', 'player', 'Adventurer', 1000, 1000, 1000, 1000, ['sword', '10 gold'])
+tavern_owner = character('NPC', 'Tavern Owner', 1000, 1000, 1000, 0, ['large knife'])
+player = character('player character', 'Adventurer', 1000, 1000, 1000, 1000, ['sword', '10 gold'])
 
 #########################################
 # LOCATION LIBRARY
 
-tavern = location((3,3), 'idle', 'Tavern', [tavern_owner], [], [], 'a sleepy tavern', [])
+forest = location('Forest', [], [], [], 'a spooky forest')
+tavern = location('Tavern', [tavern_owner], [], [], 'a sleepy tavern')
--- game_controller.py
+++ game_controller.py
@@ -1,51 +1,51 @@
 from classes import *
 from enemyspawner import *
 import random
+
+def loadScene(location, player):
+	currentLocation = location
+	currentLocation.characters.append(player)
+	list_of_characters = []
+	for l in range(0, len(currentLocation.characters)):
+		list_of_characters.append(currentLocation.characters[l].name)
+
+	print("You are in " + currentLocation.description + ". You see the " + ', and '.join(list_of_characters))
+
+
+#####################################################
+# CAUTION: LIVE TESTING
+# WEAR PROTECTIVE EQUIPMENT
 	  
-currentLocation = tavern
-currentLocation.characters.append(player)
-list_of_characters = []
-for l in range(0, len(currentLocation.characters)):
-	list_of_characters.append(currentLocation.characters[l].name)
+#currentLocation.characters.append(player)
+#list_of_characters = []
+#for l in range(0, len(currentLocation.characters)):
+#	list_of_characters.append(currentLocation.characters[l].name)
 
-print("You are in " + currentLocation.description + ". You see the " + ', and '.join(list_of_characters))
+#print("You are in " + currentLocation.description + ". You see the " + ', and '.join(list_of_characters))
 
-
-
-print('another adventurer walks in')
-currentLocation.characters.append(player)
-
-	
-print('there are currently ' + str(len(currentLocation.list_of_enemies)) + ' enemies in ' + currentLocation.name)
-
-currentLocation.list_of_enemies = spawn_zombies(random.randint(1, 6))
-	
-print('there are now ' + str(len(currentLocation.list_of_enemies)) + ' enemies in ' + currentLocation.name)
-
-for i in range(len(currentLocation.list_of_enemies)):
-	print(currentLocation.list_of_enemies[i].name)
 
 ######################################################
-# ENEMIES TEST
-
-#list_of_enemies = []
-#print(list_of_enemies)
-#print()
-
-#for i in range(0, 6):
-#	zombie = character('enemy', 'zombie', 'undead zombie', 3, 10, 5, 0, [])
 	
-#	list_of_enemies.append(zombie)
-	
-	
-#for e in list_of_enemies:
-#	print(e.health)
-#	print()
-	
-#list_of_enemies[3].health -= 6
+#print('there are currently ' + str(len(currentLocation.list_of_enemies)) + ' enemies in ' + currentLocation.name)
 
-#for e in list_of_enemies:
-#	print(e.health)
-#	print()
+#currentLocation.list_of_enemies = spawn_zombies(random.randint(1, 6))
+	
+#print('there are now ' + str(len(currentLocation.list_of_enemies)) + ' enemies in ' + currentLocation.name)
 
-##############################################
+#for i in range(len(currentLocation.list_of_enemies)):
+#	print(currentLocation.list_of_enemies[i].name)
+
+######################################################
+
+play = input("welcome to the game. Press j to play.")
+if (play == 'j'):
+	loadScene(tavern, player)
+
+play = input("press j again")
+if (play == 'j'):
+	loadScene(forest, player)
+
+play = input("press j to quit")
+if (play == 'j'):
+	quit()
+
